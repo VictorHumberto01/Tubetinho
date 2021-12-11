2db78dffa31b24cfed3b7c21b6d63b519f308b88
vitub <91441458+vitub@users.noreply.github.com>
2021-12-11 00:57:25 +0000

    Update files.py

--- files.py
+++ files.py
@@ -1,6 +1,6 @@
 import os
 
-X=os.listdir('/mnt/HDD2/Python/bot')
+X=os.listdir('path to the bot directory')
 
 for item in X:
     if item.endswith('.webm'):
@@ -9,3 +9,5 @@ for item in X:
 for item in X:
     if item.endswith('.mp3'):
         os.remove(item)
+        
+# This script removes the old music files from the bot directory. It's necessary to change the directory on the 3rd line.
